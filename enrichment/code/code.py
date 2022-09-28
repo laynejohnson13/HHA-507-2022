002fb2654f8680ac98d93f9b05f1e4576060f59f
laynejohnson13 <laynejohnson13001@gmail.com>
2022-09-28 18:55:07 -0400

    code edited

--- enrichment/code/code.py
+++ enrichment/code/code.py
@@ -14,6 +14,55 @@ medications.columns
 patients['Id']
 medications['PATIENT']
 
+#### we are going to take medications table and 
+#enrich it with some patient information from out patient table
+
+df_patients_small = patients[['Id', 'CITY', 'STATE', 'COUNTY', 'ZIP']]
+print(df_patients_small.sample(10).to_markdown())
+
+df_medications_small = medications [['PATIENT', 'CODE', 'DESCRIPTION', 'BASE_COST', 'PAYER']]
+print(df_medications_small.sample(10).to_markdown())
+
+
+combined_df = df_medications_small.merge(df_patients_small, how= 'left', left_on='PATIENT', right_on='Id')
+
+combined_df = pd.merge(df_medications_small, df_patients_small, how ='left', left_on= 'PATIENT', right_on = 'Id')
+
+combined_df.columns
+
+
+###save new combined df 
+combined_df.to_csv ('enrichment/example_data/combined_df.csv')
+
+###
+med_df = medications [['PATIENT', 'PAYER']]
+pay_df = payers_df[['Id', 'CITY']]
+pat_df = patients[[['Id', 'CITY', 'STATE', 'COUNTY', 'ZIP']]]
+
+##### ANOTHER combined df but merging the other way
+
+combined_df = df_patients_small.merge(df_medications_small, how= 'left', left_on='Id', right_on='PATIENT')
+
+
+###load in payers
+
+payers_df = pd.read_csv('enrichment/example_data/payers.csv')
+payers_df
+
+payers_df.shape
+payers_df.head(5)
+
+### creating another new df 
+
+payers_df = payers_df[['NAME', 'Id', 'AMOUNT_COVERED']]
+
+payers_df.shape
+
+patients_payers = df_patients_small.merge(payers_df_small, how = 'left', left_on = 'Id', right_on = 'Id')
+
+patients_payers.to_csv ('enrichment/example_data/patients_payers.csv')
+
+
 
 ########################
 ### merge examples 
